84d8f93ad941659e2e23dd9b2f8ad618243fe531
dnierman0920 <davidnierman93@gmail.com>
2022-02-22 14:13:58 -0800

    Create movement of Caterpillar through movementHandler and screenRefresh()

--- js/main.js
+++ js/main.js
@@ -10,17 +10,19 @@ const ctx = glassJar.getContext('2d')
 
 //create a class that will be used to create interactive elements on the screen
 class interactiveElement {
-    constructor(x,y,height,width,imgUrl, opacity) {
+    constructor(x,y,width,height,color, opacity=1) {
         this.x = x,
         this.y = y,
-        this.height = height,
         this.width = width,
-        this.imgUrl = imgUrl,
+        this.height = height,
+        this.color = color,
         this.opacity = opacity,
         this.render = function() {
-            ctx.fillStyle = 'green' // change this later
-            // ctx.fillRect will draw a rectangle on the canvas
-            ctx.fillRect(this.x, this.y, this.height, this.width)
+            ctx.fillStyle = this.color // change this later to an image instead of a color
+            ctx.fillRect(this.x, this.y,this.width, this.height)
+            // ctx.fillRect will draw a rectangle on the canvas 
+            // will be replacing this with an image --> https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+            //ctx.drawImage('../img/Caterpillar.png', 3, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
         }
         this.eatingPoints = 1,
         this.sleepingPoints = 0.5,
@@ -30,6 +32,7 @@ class interactiveElement {
             this.width *= this.sleepingPoints
         }
         this.increaseSleepPoints = function () {
+            console.log("this is running for some reasons")
             this.sleepingPoints += 0.05
             this.opacity =  sleepingPoints
         }
@@ -37,10 +40,46 @@ class interactiveElement {
 }
 
 // create Caterpillar
-let Caterpillar = new interactiveElement(100,50,10,10,"placeholder image url",1)
+let caterpillar = new interactiveElement(450,425,25,5,"green",0.5)
+let bed = new interactiveElement(400,350,100,150,"white")
+
+// create function that receives a 'keydown' and moves accordingly
+// found each key's code using this website: https://www.khanacademy.org/computer-programming/keycode-database/1902917694
+// up=38, down=40, left=37, right=39
+const movementHandler = (e) => {
+    console.log('arrow click event received, ', e.keyCode)
+    switch(e.keyCode){
+        case(38): //up arrow
+            console.log('up arrow was clicked')
+            caterpillar.y -= 10; 
+            break
+        case(40): // down arrow
+            console.log('down arrow was clicked')
+            caterpillar.y += 10;
+            break
+        case(39): // right arrow
+            console.log('right arrow was clicked')
+            caterpillar.x += 10;
+            break
+        case(37): // left arrow
+            console.log('left arrow was clicked')
+            caterpillar.x -= 10;
+            break
+    }
+    
+}
+
+// create a function that refreshes the page every 50 milliseconds to reflect the movements on the screen
+const screenRefresh = () => {
+    console.log('screen refreshed!')
+    ctx.clearRect(0,0,500,500)
+    bed.render()
+    caterpillar.render()
+}
+
 
 //add event listeners
 document.addEventListener('DOMContentLoaded', function() {
-    Caterpillar.render()
-    
+    document.addEventListener('keydown', movementHandler)
+    setInterval(screenRefresh, 50) // refresh screen every 50 ms
 })
